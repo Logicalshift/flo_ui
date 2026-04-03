528077590dd4b042b5d55a70125a4a1c0ee950af
Andrew Hunter <andrew@logicalshift.co.uk>
2026-04-03 18:13:09 +0100

    Add docs for the focus module

--- ui/src/focus/mod.rs
+++ ui/src/focus/mod.rs
@@ -1,3 +1,15 @@
+//!
+//! The standard implementation of Focus
+//!
+//! Note, you only need to know about the `Focus` and `FocusEvent` messages to interact with this
+//! subprogram. Typically there's only one Focus per window scene, so you only need to send messages
+//! to the default target when working with a window. Focus itself isn't even dependent on flo_draw
+//! so there's usually not even a need to start it manually when using with other windowing libraries.
+//!
+//! The functions in here can be used to create your own seperate Focus subprogram with independent
+//! regions and event handling, should that ever be useful.
+//!
+
 mod subprogram;
 mod button_state;
 mod regions;
